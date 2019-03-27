38fc49fe30dcb416f1919ce68a80b57e80b41a76
Nann Choi <xiaonan@inbit.tech>
2019-03-27 14:43:37 +0800

    Customize 404 Page for error route handling

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,9 @@
 @extends('errors::minimal')
 
 @section('title', __('出错啦'))
-@section('code', '404')
+
+@section('code')
+<a href="/" style="font-size: 21px; text-decoration: none;">返回</a>
+@endsection
+
 @section('message', __('出错啦'))
