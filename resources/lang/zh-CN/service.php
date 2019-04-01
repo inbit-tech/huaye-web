004b29e11023008b40155389d845dfb2f2c26541
vayne <330359351@qq.com>
2019-04-01 19:41:58 +0800

    complete header&footer and lot of multilingual package of enterprise info

---
+++ resources/lang/zh-CN/service.php
@@ -0,0 +1,9 @@
+<?php
+
+return [  
+    'Specializing in customized production, meet the special requirement of the product'=>'专业定制化生产，满足特殊产品需求',
+    'Rapid response, provide efficiency benefits'=>'快速响应机制，提供效率优势',
+    'Free sample mode, reduce customer development costs'=>'免费样品模式，降低客户研发成本',
+    'Assist customers with wire application solutions'=>'协助客户提供线材应用解决方案',
+    'Small MOQ, abundant variety, fast and accurate delivery'=>'小批量，品种丰富，快速精准交付',
+];
--- resources/views/layout/navigation/top.blade.php
+++ resources/views/layout/navigation/top.blade.php
@@ -1,6 +1,16 @@
 {{-- Top Navigation --}}
-<nav>
-	<ul>
-		<li><a href="" title=""></a></li>
-	</ul>
+<nav class="hero-head margin-top-05">
+	<div class="columns heading has-text-weight-bold">
+		<div class="column center">
+			<figure class="navbar-item image">
+				<a href="/"><img src="http://pp3tibsrj.bkt.clouddn.com/huaye-logo.png" style="width: 11rem;"  alt="logo"></a>
+			</figure>
+		</div>
+		<div class="column center">
+			<a href="/" title=""><p class="center navbar-item has-text-gray is-size-6">主页 </p></a>
+			<a href="/products" title=""><p class="center navbar-item has-text-gray is-size-6">产品</p></a>
+			<a href="/about" title=""><p class="center navbar-item has-text-gray  is-size-6">关于我们</p></a>
+			<a href="/contact" title=""><p class="center navbar-item has-text-gray is-size-6">联系我们</p></a>
+		</div>
+	</div>
 </nav>
