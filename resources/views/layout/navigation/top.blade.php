4420809f2b3f7b7369df3f993ca007eec86a8e1a
vayne yax <330359531@qq.com>
2019-04-06 18:42:41 +0800

    fix header&&footer UI

--- resources/views/layout/navigation/top.blade.php
+++ resources/views/layout/navigation/top.blade.php
@@ -1,16 +1,30 @@
 {{-- Top Navigation --}}
-<nav class="hero-head margin-top-05">
-	<div class="columns heading has-text-weight-bold">
-		<div class="column center">
-			<figure class="navbar-item image">
-				<a href="/"><img src="http://pp3tibsrj.bkt.clouddn.com/huaye-logo.png" style="width: 11rem;"  alt="logo"></a>
+<nav class="hero-head m-t-25">
+	<div class="columns has-text-weight-bold">
+		<div class="column is-2"></div>
+		<div class="column is-4">
+			<figure class="image">
+				<a href="/">
+					<img src="http://pp3tibsrj.bkt.clouddn.com/huaye-logo.png" style="width: 11rem;"  alt="logo">
+				</a>
 			</figure>
 		</div>
-		<div class="column center">
-			<a href="/" title=""><p class="center navbar-item has-text-gray is-size-6">主页 </p></a>
-			<a href="/products" title=""><p class="center navbar-item has-text-gray is-size-6">产品</p></a>
-			<a href="/about" title=""><p class="center navbar-item has-text-gray  is-size-6">关于我们</p></a>
-			<a href="/contact" title=""><p class="center navbar-item has-text-gray is-size-6">联系我们</p></a>
+		<div class="column is-4">
+			<div class="columns text-center">
+				<a class="column navbar-item" href="/" title="">
+					<p style="color:#707070;">主页</p>
+				</a>
+				<a class="column navbar-item" href="/products" title="">
+					<p style="color:#707070;">产品</p>
+				</a>
+				<a class="column navbar-item" href="/about" title="">
+					<p  style="color:#707070;">关于我们</p>
+				</a>
+				<a class="column navbar-item" href="/contact" title="">
+					<p style="color:#707070;">联系我们</p>
+				</a>
+			</div>
 		</div>
+		<div class="column is-2"></div>
 	</div>
 </nav>
