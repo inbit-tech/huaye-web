c9a9ec5eea664ac3f65bc288cc39ce207febb164
vayne yax <330359531@qq.com>
2019-04-13 02:40:12 +0800

    product home UI complete

--- resources/views/products/home.blade.php
+++ resources/views/products/home.blade.php
@@ -5,19 +5,31 @@
 @endsection
 
 @section('content')
-<section class="product-card">
-    <a href="/products/bec"><img src="https://via.placeholder.com/1920x680" alt=""></a>
+<section class="product-card has-text-centered">
+    <section class="product-card-text-section">
+        <strong class="product-title">特 种 自 粘 性 漆 包 线</strong>
+        <p class="product-des-text">用于车载设备、电声设备、传感器、VCM 马达</p>
+        <a href="/products/bec"><p class="product-ahref-text">了解更多></p></a>
+    </section>
+    <img class="product-bg-img" src="http://pp3tibsrj.bkt.clouddn.com/bec-bg.png" alt="Bonding Enameled Cooper Wire">
 </section>
 
-<section class="product-card">
-    <a href="/products/uht"><img src="https://via.placeholder.com/1920x680" alt=""></a>
+<section class="product-card has-text-centered">
+    <section class="product-card-text-section">
+        <strong class="product-title">超 高 耐 温 漆 包 线</strong>
+        <p class="product-des-text">用于电脑主板、车用电感、工业设备等</p>
+        <a href="/products/uht"><p class="product-ahref-text">了解更多></p></a>
+    </section>
+    <img class="product-bg-img" src="http://pp3tibsrj.bkt.clouddn.com/uht-bg.png" alt=" Ultra-High-Temperature Wire">
 </section>
 
-<section class="product-card">
-    <a href="/products/litz"><img src="https://via.placeholder.com/1920x680" alt=""></a>
+<section class="product-card has-text-centered">
+    <section class="product-card-text-section">
+        <strong class="product-title">LITZ 线</strong>
+        <p class="product-des-text">用于无线充电、变压器、照明系统</p>
+        <a href="/products/litz"><p class="product-ahref-text">了解更多></p></a>
+    </section>
+    <img class="product-bg-img" src="http://pp3tibsrj.bkt.clouddn.com/litz-bg.png" alt="LITZ Wire / Silk Served LITZ Wire">
 </section>
 
-<section class="product-card">
-    <a href="/products/litz"><img src="https://via.placeholder.com/1920x680" alt=""></a>
-</section>
 @endsection
