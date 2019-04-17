e8ecdd1d225e87b7e78b524e3fb167a59cb3118b
vayne yax <330359531@qq.com>
2019-04-18 00:43:57 +0800

    about-us UI complete

--- resources/views/about/home.blade.php
+++ resources/views/about/home.blade.php
@@ -10,23 +10,47 @@
         <img src="https://via.placeholder.com/1920x1008" alt="">
     </section>
 
-    <section class="quality-section has-text-centered">
-        <div class="quality-title-section">
-            <p class="quality-title">品 质 保 障</p>
+    <section class="section">
+        <div class="container">
+            <p class="has-text-centered is-size-3 has-text-grey-light">品 质 保 障</p>
         </div>
-        <div class="columns" style="width:100%;">
+    </section>
+
+    <section class="section">
+        <div class="columns">
             <div class="column is-1"></div>
-            <div class="column is-10 creed-list">
-                <div class="creed-list-item"><p class="has-text-weight-bold">信念</p></div>
-                <div class="creed-list-item"><p class="has-text-weight-bold">体系</p></div>
-                <div class="creed-list-item"><p class="has-text-weight-bold">硬件</p></div>
-                <div class="creed-list-item"><p class="has-text-weight-bold">保障</p></div>
-                <div class="creed-list-item"><p class="has-text-weight-bold">控制</p></div>
+            <div class="column is-2">
+                <div class="quality-border">
+                    <p class="is-size-2 has-text-grey has-text-weight-bold">信念</p>
+                </div>
             </div>
-            <div class="column is-1"></div>
+            <div class="column is-2">
+                <div class="quality-border">
+                    <p class="is-size-2 has-text-grey has-text-weight-bold">体系</p>
+                </div>
+            </div>
+            <div class="column is-2">
+                <div class="quality-border">
+                    <p class="is-size-2 has-text-grey has-text-weight-bold">硬件</p>
+                </div>
+            </div>
+            <div class="column is-2">
+                <div class="quality-border">
+                    <p class="is-size-2 has-text-grey has-text-weight-bold">保障</p>
+                </div>
+            </div>
+            <div class="column is-2">
+                <div class="quality-border">
+                    <p class="is-size-2 has-text-grey has-text-weight-bold">控制</p>
+                </div>
+            </div>
+            <div class="column is-1"></div> 
         </div>
-        <div class="creed-text-section has-text-left">
-            <p class="creed-text has-text-weight-bold">
+    </section>
+
+    <section class="section">
+        <div class="container has-text-left">
+            <p class="is-size-1 has-text-black-bis has-text-weight-bold">
                 <span>严格的品质控制标准及逐轴检验的控制手段，</span>
                 <br>
                 <span>严保产品的高度一致性</span>
@@ -34,91 +58,131 @@
         </div>
     </section>
 
-    <section class="history-section">
-        <p class="history-title">公 司 概 况</p>
-        <div class="columns history-text-section">
-            <div class="column is-3"></div>
-            <div class="column is-9">
-                <p class="history-text">2003 . 12 . 08 - 深圳市华冶电工材料有限公司成立</p>
-                <p class="history-text">2004 . 03 . 12 - 深圳市华冶电工材料有限公司开工投产</p>
-                <p class="history-text">2005 . 07 - 深圳市华冶电工材料有限公司获得 ISO9001 体系认证</p>
-                <p class="history-text">2009 . 07 - 深圳市华冶电工材料有限公司取得 UL 安全认证</p>
-                <p class="history-text">2010 . 08 - 深圳市华冶电工材料有限公司取得生产技术专利</p>
-                <p class="history-text">2013 . 10 . 25 - 公司整体搬迁至东莞大朗，东莞市华冶线缆科技有限公司成立</p>
-            </div>
+    <section class="section is-medium history-padding history-section-bg-img has-text-centered">
+        <p class="has-text-centered is-size-4 has-text-white history-text-shadow history-text-line-height">公 司 概 况</p>
+        <div class="history-text level section container tile is-vertical">
+            <p class="has-text-left level-left is-size-4 has-text-white history-text-shadow history-text-line-height">2003 . 12 . 08 - 深圳市华冶电工材料有限公司成立</p>
+            <p class="has-text-left level-left is-size-4 has-text-white history-text-shadow history-text-line-height">2004 . 03 . 12 - 深圳市华冶电工材料有限公司开工投产</p>
+            <p class="has-text-left level-left is-size-4 has-text-white history-text-shadow history-text-line-height">2005 . 07 - 深圳市华冶电工材料有限公司获得 ISO9001 体系认证</p>
+            <p class="has-text-left level-left is-size-4 has-text-white history-text-shadow history-text-line-height">2009 . 07 - 深圳市华冶电工材料有限公司取得 UL 安全认证</p>
+            <p class="has-text-left level-left is-size-4 has-text-white history-text-shadow history-text-line-height">2010 . 08 - 深圳市华冶电工材料有限公司取得生产技术专利</p>
+            <p class="has-text-left level-left is-size-4 has-text-white history-text-shadow history-text-line-height">2013 . 10 . 25 - 公司整体搬迁至东莞大朗，东莞市华冶线缆科技有限公司成立</p>
         </div>
     </section>
 
-    <section class="services-section has-text-centered">
-        <div class="columns">
-            <div class="column is-3"></div>
-            <p class="column is-6 has-text-centered services-title">我们尊崇真诚、团结、信任、团结、创造、分享<br>尊重客户体验，适应客户需求，快速响应，定制化生产</p>
-            <div class="column is-3"></div>
-        </div>
-            
-        <div class="columns">
+    <section class="section services-section has-text-centered">
+        <div class="section columns">
             <div class="column is-2"></div>
-            <div class="column is-8">
-                <div class="services-description-block-left">
-                    <p class="services-description-big-text has-text-weight-bold">专业</p>
-                    <img src="http://pp3tibsrj.bkt.clouddn.com/services-img-1.png" alt="advantages of Huaye">
-                </div>
-                <p class="services-description-small-text">定制化生产，满足特殊产品需求</p>   
-            </div>
+            <p class="column is-8 has-text-centered is-size-3 has-text-grey-light">我们尊崇真诚、团结、信任、团结、创造、分享<br>尊重客户体验，适应客户需求，快速响应，定制化生产</p>
             <div class="column is-2"></div>
         </div>
 
-        <div class="columns">
-            <div class="column is-2"></div>
-            <div class="column is-8">
-                <div class="services-description-block-right">
-                    <img src="http://pp3tibsrj.bkt.clouddn.com/services-img-2.png" alt="advantages of Huaye">
-                    <div class="text-right">
-                        <p class="services-description-big-text">快速</p>
-                        <p class="services-description-small-text">响应机制，提供效率优势</p>
+        <div class="section tile is-ancestor columns">
+            <div class="column is-1"></div>
+            <div class="tile is-vertical is-10">
+                <div class="tile">
+                    <div class="tile is-parent">
+                        <article class="tile is-child">
+                            <p class="services-description-big-text has-text-black has-text-weight-bold">专业</p>
+                        </article>
+                    </div>
+                    <div class="tile is-parent">
+                        <article class="tile is-child">
+                            <img class="services-description-block-img-right" src="http://pp3tibsrj.bkt.clouddn.com/services-img-1.png" alt="advantages of Huaye">
+                        </article>
                     </div>
                 </div>
+                <div class="tile is-parent">
+                    <article class="tile is-child">
+                        <p class="services-description-small-text has-text-grey">定制化生产，满足特殊产品需求</p>   
+                    </article>
                 </div>
-            <div class="column is-2"></div>
-        </div>
-
-        <div class="columns">
-            <div class="column is-2"></div>
-            <div class="column is-8">
-                <div class="services-description-block-left">
-                    <p class="services-description-big-text has-text-weight-bold">快速</p>
-                    <img src="http://pp3tibsrj.bkt.clouddn.com/services-img-3.png" alt="advantages of Huaye">
-                </div>
-                <p class="services-description-small-text">样品模式，降低客户研发成本</p>   
             </div>
-            <div class="column is-2"></div>
+            <div class="column is-1"></div>
         </div>
 
+        <div class="section tile is-ancestor columns">
+            <div class="column is-1"></div>
+            <div class="tile is-vertical is-3">
+                <div class="tile is-parent">
+                    <article class="tile is-child">
+                        <img class="services-description-block-img-left" src="http://pp3tibsrj.bkt.clouddn.com/services-img-2.png" alt="advantages of Huaye">
+                    </article>
+                </div>
+            </div>
+            <div class="tile is-parent is-7">
+                <article class="tile is-child has-text-right">
+                    <p class="services-description-big-text has-text-black has-text-weight-bold">快速</p>
+                    <p class="services-description-small-text has-text-grey">响应机制，提供效率优势</p>
+                </article>
+            </div>
+            <div class="column is-1"></div>
+        </div>
 
-        <div class="columns">
-            <div class="column is-2"></div>
-            <div class="column is-8">
-                <div class="services-description-block-right">
-                    <img src="http://pp3tibsrj.bkt.clouddn.com/services-img-4.png" alt="advantages of Huaye">
-                    <div class="text-right">
-                        <p class="services-description-big-text">协助</p>
-                        <p class="services-description-small-text">客户提供线材应用解决方案</p>
+        <div class="section tile is-ancestor columns">
+            <div class="column is-1"></div>
+            <div class="tile is-vertical is-10">
+                <div class="tile">
+                    <div class="tile is-parent">
+                        <article class="tile is-child">
+                            <p class="services-description-big-text has-text-black has-text-weight-bold">快速</p>
+                        </article>
+                    </div>
+                    <div class="tile is-parent">
+                        <article class="tile is-child">
+                            <img class="services-description-block-img-right" src="http://pp3tibsrj.bkt.clouddn.com/services-img-3.png" alt="advantages of Huaye">
+                        </article>
                     </div>
                 </div>
+                <div class="tile is-parent">
+                    <article class="tile is-child">
+                        <p class="services-description-small-text has-text-grey">样品模式，降低客户研发成本</p>   
+                    </article>
                 </div>
-            <div class="column is-2"></div>
+            </div>
+            <div class="column is-1"></div>
         </div>
 
-        <div class="columns services-description-last">
-            <div class="column is-2"></div>
-            <div class="column is-8 has-text-left">
-                <div class="services-description-block-left services-description-block-last">
-                    <p class="services-description-big-text has-text-weight-bold">小批量</p>
-                    <img src="http://pp3tibsrj.bkt.clouddn.com/services-img-5.png" alt="advantages of Huaye">
+        <div class="section tile is-ancestor columns">
+            <div class="column is-1"></div>
+            <div class="tile is-vertical is-3">
+                <div class="tile is-parent">
+                    <article class="tile is-child">
+                        <img class="services-description-block-img-left" src="http://pp3tibsrj.bkt.clouddn.com/services-img-4.png" alt="advantages of Huaye">
+                    </article>
                 </div>
-                <p class="services-description-small-text">品种丰富，快速精准交付</p>   
             </div>
-            <div class="column is-2"></div>
+            <div class="tile is-parent is-7">
+                <article class="tile is-child has-text-right">
+                    <p class="services-description-big-text has-text-black has-text-weight-bold">协助</p>
+                    <p class="services-description-small-text has-text-grey">客户提供线材应用解决方案</p>
+                </article>
+            </div>
+            <div class="column is-1"></div>
+        </div>
+
+        <div class="section tile is-ancestor columns">
+            <div class="column is-1"></div>
+            <div class="tile is-vertical is-10">
+                <div class="tile">
+                    <div class="tile is-parent">
+                        <article class="tile is-child">
+                            <p class="services-description-big-text has-text-black has-text-weight-bold">小批量</p>
+                        </article>
+                    </div>
+                    <div class="tile is-parent">
+                        <article class="tile is-child">
+                            <img class="services-description-block-img-right" src="http://pp3tibsrj.bkt.clouddn.com/services-img-5.png" alt="advantages of Huaye">
+                        </article>
+                    </div>
+                </div>
+                <div class="tile is-parent">
+                    <article class="tile is-child">
+                        <p class="services-description-small-text has-text-grey">品种丰富，快速精准交付</p>   
+                    </article>
+                </div>
+            </div>
+            <div class="column is-1"></div>
         </div>
     </section>
 </section>
