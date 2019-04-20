37c6d28abeec3dbc56bf36ee60b4ebba352d009f
vayne yax <330359531@qq.com>
2019-04-20 20:19:02 +0800

    bec page UI && optimizing mobile style

--- resources/views/products/bec.blade.php
+++ resources/views/products/bec.blade.php
@@ -5,11 +5,132 @@
 @endsection
 
 @section('content')
-<div class="flex-center position-ref full-height">
-     <div class="content">
-        <div class="title m-b-md">
-            Bonding Enameled Cooper Wire
+<section id="bec-page">
+    <section class="">
+        <img src="https://via.placeholder.com/1920x1008" alt="">
+    </section>
+
+    <section class="section is-medium container">
+        <p class="is-size-1 has-text-black has-text-weight-bold">自粘性漆包线是漆包线品种中的一个特殊品种，是在非自粘漆包线的基础上再涂覆一层自粘性涂层而成</p>
+        <div class="tile">
+            <div class="">
+                <img class="" src="http://pp3tibsrj.bkt.clouddn.com/sw-red-thick-left45.jpg" alt="Bonding Enameled Cooper Wire">
+            </div>
+            <div class="">
+                <img class="" src="http://pp3tibsrj.bkt.clouddn.com/sw-red-thick-cover.jpg" alt="Bonding Enameled Cooper Wire">
+            </div>
+            <div class="">
+                <img class="" src="http://pp3tibsrj.bkt.clouddn.com/sw-red-thick-right45.jpg" alt="Bonding Enameled Cooper Wire">
+            </div>
         </div>
-    </div>
-</div>
+    </section>
+
+    <section class="has-text-centered level is-hidden-touch">
+        <div class="level-left">
+            <p class="has-background-link has-text-white is-size-3 side-bar-text">型号标识</p>
+        </div>
+    </section>
+
+    <section class="section is-medium container">
+        <table class="table is-striped is-hoverable is-fullwidth is-bordered">
+            <thead>
+                <tr>
+                    <th class="has-text-centered has-background-link has-text-white">热熔自粘性漆包线</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th class="has-text-centered has-text-grey">LSLOCKY</th>
+                    <td>热熔低温焊锡自粘性聚氨酯漆包圆铜线，规格范围0.018mm---0.050mm。</td>
+                </tr>
+                <tr>
+                    <th class="has-text-centered has-text-grey">LSLOCKCS</th>
+                    <td>热熔低温焊锡自粘性聚氨酯漆包圆铜线，规格范围0.051mm---0.700mm。</td>
+                </tr>
+                <tr>
+                    <th class="has-text-centered has-text-grey">LOCKY</th>
+                    <td>热熔自粘性聚氨酯漆包圆铜线，规格范围0.018mm---0.050mm。</td>
+                </tr>
+                <tr>
+                    <th class="has-text-centered has-text-grey">LOCKYZ</th>
+                    <td>热熔自粘性聚氨酯漆包圆铜线，规格范围0.018mm---0.050mm。</td>
+                </tr>
+                <tr>
+                    <th class="has-text-centered has-text-grey">LOCKCS</th>
+                    <td>热熔自粘性聚氨酯漆包圆铜线，规格范围0.051mm---0.700mm。</td>
+                </tr>
+                <tr>
+                    <th class="has-text-centered has-text-grey">LOCKCZ</th>
+                    <td>热熔自粘性聚氨酯漆包圆铜线，规格范围0.051mm---0.700mm。</td>
+                </tr>
+                <tr>
+                    <th class="has-text-centered has-text-grey">SBEIW</th>
+                    <td>热熔自粘性聚酯亚胺漆包圆铜线，规格范围0.051mm---0.700mm。</td>
+                </tr>
+                <tr>
+                    <th class="has-text-centered has-text-grey">SBAIW</th>
+                    <td>热熔自粘性聚酰胺酰亚胺漆包圆铜线，规格范围0.051mm---0.700mm。</td>
+                </tr>
+                <tr>
+                    <th class="has-text-centered has-text-grey">SBEI/AIW</th>
+                    <td>热熔自粘性聚酰胺酰亚胺复合聚酯亚胺漆包圆铜线，规格范围0.051mm---0.700mm。</td>
+                </tr>
+            </tbody>
+        </table>
+    </section>
+
+    <section class="section is-medium container">
+        <table class="table is-striped is-hoverable is-fullwidth is-bordered">
+            <thead>
+                <tr>
+                    <th class="has-text-centered has-background-link has-text-white">醇溶自粘性漆包线</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th class="has-text-centered has-text-grey">LSLOCKM</th>
+                    <td>醇溶低温焊锡自粘性聚氨酯漆包圆铜线，规格范围0.018mm---0.050mm。</td>
+                </tr>
+                <tr>
+                    <th class="has-text-centered has-text-grey">LSLOCKB</th>
+                    <td>醇溶低温焊锡自粘性聚氨酯漆包圆铜线，规格范围0.051mm---0.700mm。</td>
+                </tr>
+                <tr>
+                    <th class="has-text-centered has-text-grey">LOCKM</th>
+                    <td>醇溶自粘性聚氨酯漆包圆铜线，规格范围0.018mm---0.050mm。</td>
+                </tr>
+                <tr>
+                    <th class="has-text-centered has-text-grey">LOCKMZ</th>
+                    <td>醇溶自粘性聚氨酯漆包圆铜线，规格范围0.018mm---0.050mm。</td>
+                </tr>
+                <tr>
+                    <th class="has-text-centered has-text-grey">LOCKB</th>
+                    <td>醇溶自粘性聚氨酯漆包圆铜线，规格范围0.051mm---0.700mm。</td>
+                </tr>
+                <tr>
+                    <th class="has-text-centered has-text-grey">LOCKBZ</th>
+                    <td>醇溶自粘性聚氨酯漆包圆铜线，规格范围0.051mm---0.700mm。</td>
+                </tr>
+                <tr>
+                    <th class="has-text-centered has-text-grey">DBPESVW</th>
+                    <td>醇溶/热熔自粘性聚酯漆包圆铜线，规格范围0.051mm---0.700mm。</td>
+                </tr>
+                <tr>
+                    <th class="has-text-centered has-text-grey">DBEISVW</th>
+                    <td>醇溶/热熔自粘性聚酯亚胺漆包圆铜线，规格范围0.051mm---0.700mm。</td>
+                </tr>
+            </tbody>
+        </table>
+    </section>
+
+    <section class="has-text-centered level is-hidden-touch">
+        <div class="level-left">
+            <p class="has-background-link has-text-white is-size-3 side-bar-text">结构示意图</p>
+        </div>
+    </section>
+
+    <section class="container has-text-centered">
+        <img src="http://pp3tibsrj.bkt.clouddn.com/becSketchMap.png" alt="Bonding Enameled Cooper Wire">
+    </section>
+</section>
 @endsection
