4849a60b5ac65801975f163e80da931ee119cad3
Nann Choi <xiaonan@inbit.cn>
2019-04-22 01:19:42 +0800

    Add basic lang func to site

--- resources/views/layout/navigation/hero-top.blade.php
+++ resources/views/layout/navigation/hero-top.blade.php
@@ -13,11 +13,9 @@
             </div>
             <div id="navbarMenuHeroB" class="navbar-menu">
                 <div class="navbar-end">
-                    <a class="navbar-item is-active">
-                    主页
-                    </a>
+                    <a class="navbar-item is-active">@lang('navi.home')</a>
                     <div class="navbar-item has-dropdown is-hoverable">
-                        <a class="navbar-link is-arrowless">产品</a>
+                        <a class="navbar-link is-arrowless">@lang('navi.products')</a>
 
                         <div class="navbar-dropdown is-boxed">
                             <a class="navbar-item has-text-grey-darker">特种自粘性漆包线</a>
@@ -25,8 +23,16 @@
                             <a class="navbar-item has-text-grey-darker">LITZ 线 / 丝包 LITZ 线</a>
                         </div>
                     </div>
-                    <a class="navbar-item">关于我们</a>
-                    <a class="navbar-item">联系我们</a>
+                    <a class="navbar-item">@lang('navi.about')</a>
+                    <a class="navbar-item">@lang('navi.contact')</a>
+                    <div class="navbar-item has-dropdown is-hoverable">
+                        <a class="navbar-link is-arrowless">@lang('navi.lang')</a>
+
+                        <div class="navbar-dropdown is-boxed">
+                            <a href="lang/en" class="navbar-item has-text-grey-darker">英文</a>
+                            <a href="lang/zh" class="navbar-item has-text-grey-darker">中文</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
