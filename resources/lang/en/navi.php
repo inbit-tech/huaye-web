09fbf3efad68ca8269e431e8155b341812b1770d
Nann <xiaonan@inbit.tech>
2019-04-28 03:03:46 +0800

    Update lang for footer section

--- resources/lang/en/navi.php
+++ resources/lang/en/navi.php
@@ -1,12 +1,20 @@
 <?php
 
 return [
-    'title' 	=>	'Huaye Cables Technology Co., Ltd',
-    'products'	=>	'Products',
-    'prod1'		=>	'Self Bonding Wire',
-    'prod2'		=>	'Ultra-High Temperature Resistance Wire',
-    'prod3'		=> 	'LITZ Wire',
-    'about'		=>	'About Us',
-    'contact'	=>	'Contact Us',
-    'lang'		=>	'Language'
+    'title' 			=>	'Huaye Cables Technology Co., Ltd',
+    'products'			=>	'Products',
+    'products-bottom'	=>	'Our Products',
+    'prod1'				=>	'Self Bonding Wire',
+    'prod2'				=>	'Ultra-High Temperature Wire',
+    'prod3'				=> 	'LITZ Wire',
+    'about'				=>	'About Us',
+    'contact'			=>	'Contact Us',
+    'lang'				=>	'Language',
+    'service'			=>	'Service Philosophy',
+    'quality'			=>	'Quality Assurance',
+    'intro'				=> 	'Huaye Overview',
+    'phone'             =>  'Phone: +86-769-82236800',
+    'fax'               =>  'Fax: +86-769-82236803',
+    'mail'              =>  'Mail: huaye@huaye-dg.com',
+    'more'              =>  'Learn More'
 ];
--- resources/lang/zh/navi.php
+++ resources/lang/zh/navi.php
@@ -1,12 +1,20 @@
 <?php
 
 return [
-	'title'		=>	'华冶科技 - 东莞华冶线缆科技有限公司',
-    'products'	=>	'产品',
-    'prod1'		=>	'自粘性漆包线',
-    'prod2'		=>	'超耐高温漆包线',
-    'prod3'		=> 	'LITZ线',
-    'about'		=>	'关于我们',
-    'contact'	=>	'联系我们',
-    'lang'		=>	'切换语言 '
+	'title'				=>	'华冶科技 - 东莞华冶线缆科技有限公司',
+    'products'			=>	'产品',
+    'products-bottom'	=>	'我们的产品',
+    'prod1'				=>	'自粘性漆包线',
+    'prod2'				=>	'超耐高温漆包线',
+    'prod3'				=> 	'LITZ线',
+    'about'				=>	'关于我们',
+    'contact'			=>	'联系我们',
+    'lang'				=>	'切换语言 ',
+    'service'			=>	'服务理念',
+    'quality'			=>	'质量保障',
+    'intro'				=> 	'公司概况',
+    'phone'             =>  '电话: 0769-82236800',
+    'fax'               =>  '传真: 0769-82236803',
+    'mail'              =>  '邮件: huaye@huaye-dg.com',
+    'more'              =>  '了解更多'
 ];
