e4d78e693b446dd439d861c0cdf81a5cd2de9c95
vayne yax <330359531@qq.com>
2019-05-04 00:40:32 +0800

    map init -> asynchronous load

--- public/js/app.js
+++ public/js/app.js
@@ -13921,21 +13921,22 @@ var typed = new typed_js__WEBPACK_IMPORTED_MODULE_0___default.a('#typed', {
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-var map = new AMap.Map('container', {
-  zoom: 13,
-  center: [114.005383, 22.893244],
-  viewMode: '3D'
-});
-var marker = new AMap.Marker({
-  position: new AMap.LngLat(114.005383, 22.893244),
-  title: '裕元工业园精成二路灵狮小镇C栋三层C313-316号'
-});
-marker.setLabel({
-  offset: new AMap.Pixel(20, -10),
-  content: '<div style="font-size:16px">精成二路灵狮小镇C栋三层C313-316号</div>',
-  direction: 'top'
-});
-map.add(marker);
+window.init = function () {
+  var map = new AMap.Map('container', {
+    center: [114.005383, 22.893244],
+    zoom: 13
+  });
+  var marker = new AMap.Marker({
+    position: new AMap.LngLat(114.005383, 22.893244),
+    title: '裕元工业园精成二路灵狮小镇C栋三层C313-316号'
+  });
+  marker.setLabel({
+    offset: new AMap.Pixel(20, -10),
+    content: '<div style="font-size:16px">精成二路灵狮小镇C栋三层C313-316号</div>',
+    direction: 'top'
+  });
+  map.add(marker);
+};
 
 /***/ }),
 
